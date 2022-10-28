1c87e747117445b08908bb98078396e35e37d3a7
Vikas Movva <vikas.s.movva@gmail.com>
2022-10-28 11:02:20 -0400

    add canvas and clear btn

--- src/components/demo/Demo.jsx
+++ src/components/demo/Demo.jsx
@@ -1,7 +1,56 @@
-import React from "react";
-import { DemoContainer } from "./demoElements";
-const Demo = () => {
-  return <DemoContainer></DemoContainer>;
-};
+import { React, useRef } from "react";
+import {
+  DemoContainer,
+  DemoHeader,
+  Button,
+  ButtonBg,
+  ButtonText,
+  CanvasWrapper,
+} from "./demoElements";
+import { ReactSketchCanvas } from "react-sketch-canvas";
 
+const Demo = () => {
+  const canvasStyles = {
+    gridArea: "1 / 1 / 3 / 3",
+    // marginTop: "50%",
+  };
+  const canvasRef = useRef(null);
+  return (
+    <DemoContainer>
+      <DemoHeader>MNIST Demo</DemoHeader>
+      <CanvasWrapper>
+        <ReactSketchCanvas
+          width="100%"
+          height="100%"
+          style={canvasStyles}
+          ref={canvasRef}
+          strokeColor="black"
+          strokeWidth={20}
+        />
+        <DemoButton
+          rotation={13}
+          column={3}
+          row={1}
+          text="Clear"
+          canvas={canvasRef}
+        />
+      </CanvasWrapper>
+    </DemoContainer>
+  );
+};
+const DemoButton = (props) => {
+  return (
+    <ButtonBg rotation={props.rotation} column={props.column} row={props.row}>
+      <Button rotation={-props.rotation}>
+        <ButtonText
+          onClick={() => {
+            props.canvas.current.clearCanvas();
+          }}
+        >
+          {props.text}
+        </ButtonText>
+      </Button>
+    </ButtonBg>
+  );
+};
 export default Demo;
