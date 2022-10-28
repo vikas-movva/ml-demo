4a4f17a9e7aac042c4be53dfaddba923dc9e78c9
Vikas Movva <vikas.s.movva@gmail.com>
2022-10-27 21:35:12 -0400

    remove console log

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Demo from "./components/demo/Demo";
 function App() {
-  console.log(window.location.href);
   return (
     <div className="App">
       <Sidebar />
