454d9f32660211874dd76a8e0d9f1459283313e4
Vikas Movva <vikas.s.movva@gmail.com>
2022-10-27 21:45:59 -0400

    add constants.js for constants

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const primaryColor = "rgba(255, 245, 245, 1)";
+export const secondaryColor = "rgba(26, 30, 38, 1)";
+export const buttonPrimaryColor = "rgba(255, 245, 245, 0.75)";
+export const buttonSecondaryColor = "rgba(115, 117, 120, 1)";
+export const buttonActiveColor = "rgba(223, 148, 79, 1)";
