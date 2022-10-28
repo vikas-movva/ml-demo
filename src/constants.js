b20dd872b6e4548a00025c32b891130d8f6fd35b
Vikas Movva <vikas.s.movva@gmail.com>
2022-10-28 09:51:49 -0400

    add rotation to constants

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,4 @@ export const secondaryColor = "rgba(26, 30, 38, 1)";
 export const buttonPrimaryColor = "rgba(255, 245, 245, 0.75)";
 export const buttonSecondaryColor = "rgba(115, 117, 120, 1)";
 export const buttonActiveColor = "rgba(223, 148, 79, 1)";
+export const rotation = 12;
